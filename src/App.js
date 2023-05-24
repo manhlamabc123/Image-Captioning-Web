4b84e89c97a88c285dd641a86743027979f09036
manhlamabc123 <tranducmanh12092001@gmail.com>
2023-05-24 10:04:44 +0700

    Udpate remove uploaded image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef } from 'react';
 import axios from 'axios';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -6,6 +6,7 @@ function App() {
   const [image, setImage] = useState(null);
   const [link, setLink] = useState('');
   const [response, setResponse] = useState(null);
+  const fileInputRef = useRef(null);
 
   const handleImageChange = (e) => {
     setImage(e.target.files[0]);
@@ -36,19 +37,47 @@ function App() {
     }
   };
 
+  const handleRemoveImage = () => {
+    setImage(null);
+    if (fileInputRef.current) {
+      fileInputRef.current.value = null;
+    }
+  };
+
   return (
     <div className="container">
-      <h1 className="my-4">Image Uploader</h1>
+      <h1 className="my-4">Image Captioning</h1>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="image" className="form-label">Image:</label>
-          <input type="file" className="form-control" id="image" accept="image/*" onChange={handleImageChange} disabled={link !== ''} />
+          <input
+            type="file"
+            className="form-control"
+            id="image"
+            accept="image/*"
+            onChange={handleImageChange}
+            ref={fileInputRef}
+          />
+          {image && (
+            <button type="button" className="btn btn-danger mt-2" onClick={handleRemoveImage}>
+              Remove Image
+            </button>
+          )}
         </div>
         <div className="mb-3">
           <label htmlFor="link" className="form-label">Link:</label>
-          <input type="text" className="form-control" id="link" value={link} onChange={handleLinkChange} disabled={image !== null} />
+          <input
+            type="text"
+            className="form-control"
+            id="link"
+            value={link}
+            onChange={handleLinkChange}
+            disabled={image !== null}
+          />
         </div>
-        <button type="submit" className="btn btn-primary" disabled={!image && !link}>Submit</button>
+        <button type="submit" className="btn btn-primary" disabled={!image && !link}>
+          Submit
+        </button>
       </form>
 
       {response && (
